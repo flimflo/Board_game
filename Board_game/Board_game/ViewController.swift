df55d9e5122b72b098c72003eb590b73f26a2cf9
Fernando Limón Flores <flimflo2@hotmail.com>
2019-03-23 10:49:33 -0600

    primer commit

---
+++ Board_game/Board_game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Board_game
+//
+//  Created by Fernando Limón Flores on 3/23/19.
+//  Copyright © 2019 Fernando Limón Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
