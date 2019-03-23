10b405c0782b0c6993c22acc3bbf373f3eda4893
Fernando Limón Flores <flimflo2@hotmail.com>
2019-03-23 13:03:45 -0600

    Cambios en startView

---
+++ Board_game/Board_game/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  Board_game
+//
+//  Created by Fernando Limón Flores on 3/23/19.
+//  Copyright © 2019 Fernando Limón Flores. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
