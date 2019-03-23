9ff44af32f5ca5e2aa5c79fdaf4cda2180d509b2
Juan De Leon <juandeleon@Juans-MacBook-Air.local>
2019-03-23 10:56:07 -0600

    Test

--- Board_game/Board_game/ViewController.swift
+++ Board_game/Board_game/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //commit test
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
